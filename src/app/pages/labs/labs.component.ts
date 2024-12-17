07e695001da1d7ea91e3e76954d46b79b2ef96f3
Elisa <elisa.santibanez1994@gmail.com>
2024-12-16 22:39:44 -0300

    se agregan eventos
    se agregan diferentes tipos de eventos como click, dblclick, change y keydown

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { compileNgModule } from '@angular/compiler';
 
 
 @Component({
@@ -7,8 +8,9 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrls: ['./labs.component.css']
 })
+
 export class LabsComponent {
   Welcome = 'Quiero que se muestre en el html';
   tasks = [
@@ -18,6 +20,28 @@ export class LabsComponent {
   ]
   name = 'Elisa';
   age = '30';
+  disabled = true;
+  img ='https://www.webconsultas.com/sites/default/files/styles/wc_adaptive_image__medium/public/media/2024/08/06/munchkin_gato.jpg.webp'
 
+  person = {
+    name: 'Elisa',
+    age: 18,
+    office: 'Desarrolladora web'
+
+  }
+
+  //metodo de la clase
+  clickHandler() {
+    alert('hola')
+  }
+
+  changeHandler(event: Event) {
+    console.log(event)
+  }
+
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value)
+  }
 
 }
